fd76357b153080a8d76944f757954817f3ea6920
csprock <csprock@gmail.com>
2021-11-19 11:01:23 -0800

    added compute_transition_matrix() and test. Updated tests to use Matrix::Matrix

--- R/matrix_functions.R
+++ R/matrix_functions.R
@@ -6,3 +6,7 @@ matrix_power <- function(x, k, ...) {
 matrix_summation <- function(mat_list) {
   Reduce(`+`, mat_list, accumulate = FALSE)
 }
+
+compute_transition_matrix <- function(M) {
+    M / Matrix::rowSums(M)
+}
--- tests/testthat/test-matrix_functions.R
+++ tests/testthat/test-matrix_functions.R
@@ -1,20 +1,43 @@
 test_that("Test matrix_power()", {
   
-  M <- Matrix(2, 2, 2)
+  M <- Matrix::Matrix(2, 2, 2)
 
   results_single <- matrix_power(M, 2, accumulate = FALSE)
   results_multiple <- matrix_power(M, 2, accumulate = TRUE)
 
-  expect_equal(sum(Matrix(8, 2, 2) == results_single), 4)
-  expect_equal(sum(Matrix(2, 2, 2) == results_multiple[[1]]), 4)
-  expect_equal(sum(Matrix(8, 2, 2) == results_multiple[[2]]), 4)
+  expect_equal(sum(Matrix::Matrix(8, 2, 2) == results_single), 4)
+  expect_equal(sum(Matrix::Matrix(2, 2, 2) == results_multiple[[1]]), 4)
+  expect_equal(sum(Matrix::Matrix(8, 2, 2) == results_multiple[[2]]), 4)
 
 })
 
 test_that("Test matrix_summation()", {
-  
-  M <- Matrix(1, 2, 2)
+
+  M <- Matrix::Matrix(1, 2, 2)
   results <- matrix_summation(list(M, M))
 
-  expect_equal(sum(Matrix(2,2,2) == results), 4)
+  expect_equal(sum(Matrix::Matrix(2,2,2) == results), 4)
+})
+
+
+test_that("Test compute_transition_matrix", {
+
+  m <- Matrix::Matrix(
+    c(
+      c(1, 1, 0),
+      c(0, 1, 0),
+      c(2, 3, 0)
+    ), ncol = 3, byrow = TRUE
+  )
+
+  expected <- Matrix::Matrix(
+    c(
+      c(1/2, 1/2, 0),
+      c(0, 1, 0),
+      c(2/5, 3/5, 0)
+    ), ncol = 3, byrow = TRUE
+  )
+
+  expect_equal(sum(compute_transition_matrix(m) == expected), 9)
+
 })
