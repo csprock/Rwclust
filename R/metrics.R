c2ee36eb3845e43290f149f0767cc0b6eb162627
csprock <csprock@gmail.com>
2021-11-21 10:23:21 -0800

    added similarity application functions

--- R/metrics.R
+++ R/metrics.R
@@ -5,3 +5,19 @@ hk_similarity <- function(x, y, k) {
     exp(2*k - l1_norm(x, y) - 1)
 }
 
+
+apply_similarity<- function(idx, mat, similarity, ...) {
+  similarity(mat[idx[1]], mat[idx[2]], ...)
+}
+
+
+compute_kernel <- function(edgelist, mat, similarity, ...) {
+  apply(
+    X = edgelist,
+    FUN = apply_similarity,
+    MARGIN = 1,
+    mat = mat, 
+    similarity = similarity, 
+    ...
+  )
+}
