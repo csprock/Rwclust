8cfe483edc3fb519f188706e46146d420fc1bfbe
csprock <csprock@gmail.com>
2021-11-26 10:00:43 -0800

    fixed implementation bugs in hk_similarity() and apply_similarity()

--- R/metrics.R
+++ R/metrics.R
@@ -2,12 +2,12 @@
 l1_norm <- function(x,y) {sum(abs(x - y))}
 
 hk_similarity <- function(x, y, k) {
-    exp(2*k - l1_norm(x, y) - 1)
+    exp(2*k - l1_norm(x, y)) - 1
 }
 
 
 apply_similarity<- function(idx, mat, similarity, ...) {
-  similarity(mat[idx[1]], mat[idx[2]], ...)
+  similarity(mat[idx[1],], mat[idx[2],], ...)
 }
 
 
