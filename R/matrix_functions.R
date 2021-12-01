29ca2ad966ad1e7688ee3760afe4da97e9600860
csprock <csprock@gmail.com>
2021-11-30 21:51:55 -0800

    added compute_new_weights() and update_weights() functions

--- R/matrix_functions.R
+++ R/matrix_functions.R
@@ -7,13 +7,36 @@ matrix_summation <- function(mat_list) {
   Reduce(`+`, mat_list, accumulate = FALSE)
 }
 
+# regexpr(text = class(M), pattern = "^[a-zA-z]+Matrix$") == 1
 compute_transition_matrix <- function(M) {
-    if (is.matrix(M)) {
-      return(M / apply(X = M, MARGIN = 1, FUN = sum))
-    } else if (regexpr(text = class(M), pattern = "^[a-zA-z]+Matrix$") == 1) {
-      return(M / Matrix::rowSums(M))
-    } else {
-      stop("Must supply this function with matrix")
-    }
+      M / Matrix::rowSums(M)
 }
 
+
+update_weights <- function(M, el, similarity, k) {
+  
+  M <- compute_transition_matrix(M)
+  Mk <- matrix_summation(
+    matrix_power(M, k, accumulate = TRUE)
+  )
+  
+  weights <- compute_kernel(el, Mk, similarity = similarity, k = k)
+  adj <- construct_kernel(el, weights, symmetric = TRUE, check = FALSE)
+
+  return(list(weights = weights, adj = adj))
+
+}
+
+compute_new_weights <- function(M, el, similarity, k, iter) {
+
+  if (!is.numeric(iter) || iter < 1) {
+    stop("Invalid value for iter")
+  }
+
+  for (i in 1:iter) {
+    results <- update_weights(M = M, el = el, similarity = similarity, k = k)
+    M <- results$adj
+  }
+
+  return(results)
+}
