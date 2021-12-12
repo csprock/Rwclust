3e4eb6ab68e536fe6f8fd30acffaa1c977feb6cb
csprock <csprock@gmail.com>
2021-12-11 21:25:01 -0800

    moved input checking to separate function, fixed error in duplication check and fixed naming conflict with simple graph checking

--- R/rwclust.R
+++ R/rwclust.R
@@ -12,7 +12,7 @@ enforce_upper_triangular <- function(el) {
 }
 
 
-check_simple <- function(el) {
+check_simple_df <- function(el) {
     has_dupes <- apply(X=el, MARGIN=1, FUN=function(x){x[1]==x[2]})
     if (sum(has_dupes) > 0){
         stop("Self-loops are not allowed.")
@@ -20,7 +20,7 @@ check_simple <- function(el) {
 }
 
 check_duplicates <- function(el) {
-  if (duplicated(el) > 0) {
+  if (sum(duplicated(el)) > 0) {
     stop("Duplicated / multi-edges are not allowed.")
   }
 }
@@ -33,14 +33,28 @@ check_df_dims <- function(el) {
 }
 
 
-rwclust <- function(x, similarity="hk",...) {
+check_simple <- function(g) {
+  if (!igraph::is.simple(g)) {
+    stop("Graph must be simple")
+  }
+}
+
+check_undirected <- function(g) {
+  if (igraph::is.directed(g)) {
+    stop("Graph must be undirected")
+  }
+}
+
+
+
+rwclust <- function(x, similarity="hk", iter, k) {
 
     if (is.data.frame(x) || is.matrix(x)) {
         x <- as.matrix(x)
         check_df_dims(x)
         x[, c(1,2)] <- enforce_upper_triangular(x[, c(1,2)])
         check_duplicates(x)
-        check_simple(x)
+        check_simple_df(x)
 
         adj <- Matrix::sparseMatrix(
             i = x[,1],
@@ -48,16 +62,23 @@ rwclust <- function(x, similarity="hk",...) {
             x = x[,3],
             symmetric=TRUE
         )
+
+        return(rwclust_(adj, x, similarity, iter, k))
+
     } else {
         stop("x must be a 3-column data frame or matrix")
     }
+}
+
+
+rwclust_ <- function(adj, edgelist, similarity="hk", iter, k) {
 
     similarity <- switch(
         similarity,
         "hk" = hk_similarity
     )
 
-    sharpened_weights <- compute_new_weights(adj, x, similarity = similarity, ...)
+    sharpened_weights <- compute_new_weights(adj, edgelist, similarity, k, iter)
 
     output <- list(
         weights = sharpened_weights$weights,
