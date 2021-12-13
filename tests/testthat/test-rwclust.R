f2551f18531142bd0fe60659e48e8c282996ea92
csprock <csprock@gmail.com>
2021-12-12 19:17:20 -0800

    added test for rwclust() main function to make sure it runs on valid input

--- tests/testthat/test-rwclust.R
+++ tests/testthat/test-rwclust.R
@@ -1,3 +1,24 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that(
+  "rwclust runs error-free with valid input", 
+{
+  
+  df <- data.frame(
+    from = c(1,2,3),
+    to = c(2,3,1),
+    weight = c(1,1,1)
+  )
+
+  output <- rwclust(df, iter=3, k=1)
+  expect_type(output, "list")
+
+  if (requireNamespace("igraph", quietly = TRUE)) {
+
+    g <- igraph::graph_from_edgelist(as.matrix(df[, c(1,2)]), directed=FALSE)
+    igraph::E(g)$weights <- df$weight
+
+    output_g <- rwclust(g, iter=3, k=1)
+    expect_type(output_g, "list")
+
+  }
+
 })
