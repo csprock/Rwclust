fb10f6f5484c1bbdba1c967e2c7f3ddb6b4f2268
csprock <csprock@gmail.com>
2021-12-12 19:52:54 -0800

    added example data files

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,20 +1,18 @@
 ## code to prepare `DATASET` dataset goes here
-library(Matrix)
 
-convert_to_Matrix <- function(data) {
-  n <- max(data$from, data$to)
-  M <- Matrix(0, nrow=n, ncol=n)
-  for (k in 1:nrow(data)) {
-    M[data$from[k], data$to[k]] <- data$weight[k]
-    M[data$to[k], data$from[k]] <- data$weight[k]
-  }
-  return(M)
-}
+# library(Matrix)
 
-graph1_data <- read.csv("data-raw/example1.csv", header=TRUE)
-graph1_data <- read.csv("data-raw/example2.csv", header=TRUE)
+# convert_to_Matrix <- function(data) {
+#   n <- max(data$from, data$to)
+#   M <- Matrix(0, nrow=n, ncol=n)
+#   for (k in 1:nrow(data)) {
+#     M[data$from[k], data$to[k]] <- data$weight[k]
+#     M[data$to[k], data$from[k]] <- data$weight[k]
+#   }
+#   return(M)
+# }
 
-example1_mat <- convert_to_Matrix(graph1_data)
-example2_mat <- convert_to_Matrix(graph1_data)
+example1 <- read.csv("data-raw/example1.csv", header=TRUE)
+example2 <- read.csv("data-raw/example2.csv", header=TRUE)
 
-usethis::use_data(example1_mat, example2_mat, overwrite = TRUE)
+usethis::use_data(example1, example2, overwrite = TRUE)
