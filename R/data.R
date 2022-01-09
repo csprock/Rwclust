cae2f34290d34f1d7d215af81c3af80cbbfda3bb
csprock <csprock@gmail.com>
2022-01-08 20:35:53 -0800

    added man/ folder and documented example data

---
+++ R/data.R
@@ -0,0 +1,45 @@
+#' Example Graph 1
+#' 
+#' Second demonstration test graph used in the original paper by Harel and Koren.
+#' 
+#' @source \href{Harel, David, and Yehuda Koren. "On clustering using random walks." 
+#' International Conference on Foundations of Software Technology 
+#' and Theoretical Computer Science. Springer, Berlin, Heidelberg, 2001.}{
+#' https://link.springer.com/chapter/10.1007/3-540-45294-X_3}
+#' 
+#' @usage
+#' \dontrun{
+#' data(example1, package="Rwclust")
+#' }
+#' @format A data frame with three columns representing a weighted graph. Each
+#' row represents an edge with a weight:
+#' \describe{
+#'  \item{from}{An integer vertex id}
+#'  \item{to}{An integer vertex id}
+#'  \item{weight}{A double representing the edge weight}
+#' }
+"example1"
+
+
+#' Example Graph 2
+#' 
+#' Second demonstration test graph used in the original paper by Harel and Koren.
+#' 
+#' @source \href{Harel, David, and Yehuda Koren. "On clustering using random walks." 
+#' International Conference on Foundations of Software Technology 
+#' and Theoretical Computer Science. Springer, Berlin, Heidelberg, 2001.}{
+#' https://link.springer.com/chapter/10.1007/3-540-45294-X_3}
+#' 
+#' @usage
+#' \dontrun{
+#'  data(example2, package="Rwclust")
+#' }
+#' 
+#' @format A data frame with three columns representing a weighted graph. Each
+#' row represents an edge with a weight.
+#' \describe{
+#'  \item{from}{An integer vertex id}
+#'  \item{to}{An integer vertex id}
+#'  \item{weight}{A double representing the edge weight}
+#' }
+"example2"
