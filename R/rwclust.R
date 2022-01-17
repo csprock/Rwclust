960273f1cfa79039132e30ae2781ff65b16bf58b
csprock <csprock@gmail.com>
2022-01-16 20:40:45 -0800

    added documentation to rwclust function

--- R/rwclust.R
+++ R/rwclust.R
@@ -1,4 +1,44 @@
 
+#' Sharpen the edge weights of a weighted graph.
+#' 
+#' Sharpens the weights of a weighted graph for later pruning.
+#' 
+#' @section Details:
+#' Internally, the edgelist passed to \code{rwclust} is converted 
+#' into a transition matrix, whose powers are used to compute the 
+#' probability of reaching a vertex \eqn{u} from vertex \eqn{v} in 
+#' \eqn{k} steps for all \eqn{v,u}. New edge weights are computed 
+#' using the similarity between these "walk probabilities" for each pair
+#' of vertices. The intuition is that vertices who have similar 
+#' neighborhoods in terms of random walk reachability are similar 
+#' to each other. 
+#' 
+#' The returned weights can be used for clustering by deleting 
+#' edges with weights below a certain threshold. The connected 
+#' components of the resulting graph form the clusters. 
+#' 
+#' @references
+#' Harel, David, and Yehuda Koren. "On clustering using random walks." 
+#' International Conference on Foundations of Software Technology 
+#' and Theoretical Computer Science. Springer, Berlin, Heidelberg, 2001.
+#' 
+#' @param x matrix or dataframe with three columns
+#' \enumerate{
+#'  \item vertex label (integer)
+#'  \item vertex label (integer)
+#'  \item edge weights (float)
+#' }
+#' @param similarity string, the name of the similarity metric used to 
+#' update weights
+#' @param iter integer, number of iterations
+#' @param k integer, maximum length of random walk
+#' 
+#' @return list
+#' \describe{
+#'  \item{weights}{A vector of the updated edge weights}
+#'  \item{adjacency}{Updated adjacency matrix containing updated weights}
+#' }
+#' @export
 rwclust <- function(x, similarity="hk", iter, k) {
 
     if (is.data.frame(x) || is.matrix(x)) {
