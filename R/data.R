7b58eb191f7aca54d4cdd178bb7694bf0d6270d9
csprock <csprock@gmail.com>
2022-05-12 17:32:04 -0600

    move references to the paper from the data documentation into the DESCRIPTION file. Removed dontrun{} in data documentation. added CRAN-SUBMISSION to .Rbuildignore

--- R/data.R
+++ R/data.R
@@ -1,19 +1,10 @@
 #' Example Graph 1
-#' 
-#' Second demonstration test graph used in the original paper by Harel and Koren.
-#' 
-#' @source \url{https://link.springer.com/chapter/10.1007/3-540-45294-X_3}
-#' 
-#' @references
-#' Harel, David, and Yehuda Koren. "On clustering using random walks." 
-#' International Conference on Foundations of Software Technology 
-#' and Theoretical Computer Science. Springer, Berlin, Heidelberg, 2001.
-#' 
+#'
+#' First demonstration test graph used in the original.
+#'
 #' @examples
-#' \dontrun{
 #' data(example1, package="Rwclust")
-#' }
-#' 
+#'
 #' @format A data frame with three columns representing a weighted graph. Each
 #' row represents an edge with a weight:
 #' \describe{
@@ -25,21 +16,12 @@
 
 
 #' Example Graph 2
-#' 
-#' Second demonstration test graph used in the original paper by Harel and Koren.
-#' 
-#' @source \url{https://link.springer.com/chapter/10.1007/3-540-45294-X_3}
-#' 
-#' @references
-#' Harel, David, and Yehuda Koren. "On clustering using random walks." 
-#' International Conference on Foundations of Software Technology 
-#' and Theoretical Computer Science. Springer, Berlin, Heidelberg, 2001.
-#' 
+#'
+#' Second demonstration test graph used in the original paper.
+#'
 #' @examples
-#' \dontrun{
-#'  data(example2, package="Rwclust")
-#' }
-#' 
+#' data(example2, package="Rwclust")
+#'
 #' @format A data frame with three columns representing a weighted graph. Each
 #' row represents an edge with a weight.
 #' \describe{
